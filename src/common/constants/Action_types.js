846f9b599a224febd6711c464139b2a8f49675dd
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-04-28 13:26:12 +0700

    add action type

---
+++ src/common/constants/Action_types.js
@@ -0,0 +1,7 @@
+const actionTypes = {
+  GetGroupRequested: 'GET_GROUP_REQUESTED',
+  GetGroupRejected: 'GET_GROUP_REJECTED',
+  GetGroupFulfilled: 'GET_GROUP_FULFILLED',
+};
+
+export default actionTypes;
